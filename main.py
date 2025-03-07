38df0f7482d0e5e03ea8272dd561958f31be7b40
Mike Woodhouse <mikewoodhouse@gmail.com>
2025-03-07 12:25:41 +0000

    refactor: pasas cards into display - allows part-/fully-complete cards updated externally to be displayed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from nicegui import ui
 
+from ec2.scorebook import ScoreCard
 from src.ec2.ui.display import Display
 
-display = Display()
+display = Display(card_1=ScoreCard(), card_2=ScoreCard())
 
 display.show()
 
